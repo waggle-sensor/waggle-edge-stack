4907675e5c8cac6374306111f95b0d7d342bf004
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-24 16:06:14 -0600

    added upload server for testing. define-test-credentials.sh uses -e flag now.

--- kubernetes/define-test-credentials.sh
+++ kubernetes/define-test-credentials.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # defines made up credentials for testing. this includes:
 # * Node ID
@@ -7,17 +7,37 @@
 # * Node SSH key pair
 # * Signed Node SSH cert
 
+tempdir=$(mktemp -d)
+cd "$tempdir"
+echo "working in $(pwd)"
+
 WAGGLE_NODE_ID="0000000000000001"
 
-# generate keys. remember, we will only have presigned keys and only ca.pub!
-ssh-keygen -C "Waggle CA Key" -N '' -f ca
-ssh-keygen -C "Waggle SSH Key" -N '' -f ssh-key
+# generate node configmap
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: waggle-config
+data:
+  WAGGLE_NODE_ID: "$WAGGLE_NODE_ID"
+  WAGGLE_UPLOAD_HOST: "beehive-upload-server"
+EOF
+
+# TODO split ca.pub into its own thing and then just use generic signed ssh key resource?
+
+# generate test ca key pair
+ssh-keygen -C "Beekeeper CA Key" -N "" -f ca
+
+# generate and sign node ssh key
+# do we need different access between beekeeper and the upload server??
+ssh-keygen -C "Node SSH Key" -N "" -f node-ssh-key
 
 ssh-keygen -s ca \
     -I "Waggle Upload Key" \
     -n "node$WAGGLE_NODE_ID" \
     -V "-5m:+7d" \
-    ssh-key
+    node-ssh-key
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -27,20 +47,30 @@ metadata:
   name: waggle-secret
 data:
   ca.pub: $(base64 -w 0 ca.pub)
-  ssh-key: $(base64 -w 0 ssh-key)
-  ssh-key.pub: $(base64 -w 0 ssh-key.pub)
-  ssh-key-cert.pub: $(base64 -w 0 ssh-key-cert.pub)
+  ssh-key: $(base64 -w 0 node-ssh-key)
+  ssh-key.pub: $(base64 -w 0 node-ssh-key.pub)
+  ssh-key-cert.pub: $(base64 -w 0 node-ssh-key-cert.pub)
 EOF
 
-# clean up test key files. (they are copied into the secret)
-rm ca ca.pub ssh-key ssh-key.pub ssh-key-cert.pub
+# generate and sign
+ssh-keygen -C "Upload Server Key" -N "" -f upload-server-host-key
+
+ssh-keygen -s ca \
+    -I "Upload Server Key" \
+    -n "upload-server" \
+    -V "-5m:+7d" \
+    -h \
+    upload-server-host-key
 
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
-kind: ConfigMap
+kind: Secret
+type: Opaque
 metadata:
-  name: waggle-config
+  name: beehive-upload-server-secret
 data:
-  WAGGLE_NODE_ID: "$WAGGLE_NODE_ID"
-  WAGGLE_UPLOAD_HOST: "upload-server"
+  ca.pub: $(base64 -w 0 ca.pub)
+  upload-server-key: $(base64 -w 0 upload-server-host-key)
+  upload-server-key.pub: $(base64 -w 0 upload-server-host-key.pub)
+  upload-server-key-cert.pub: $(base64 -w 0 upload-server-host-key-cert.pub)
 EOF
