ccaa785af70678195b12f3acef74845612d0df38
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-12-08 10:47:50 -0600

    clean up node exporter

--- kubernetes/delete-stack.sh
+++ kubernetes/delete-stack.sh
@@ -6,6 +6,7 @@ kubectl delete -f playback-server
 kubectl delete -f rabbitmq-server
 kubectl delete -f beehive-upload-server
 kubectl delete -f data-sharing-service.yaml
+kubectl delete -f node-exporter.yaml
 
 # delete config and secrets
 kubectl delete configmap waggle-config
