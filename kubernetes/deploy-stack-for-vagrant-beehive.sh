d476f0e23380c1406399d9b5e008f4ea40f9b272
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-10 10:29:29 -0600

    added config for honeyhouse and vagrant deployments. fixed node exporter listen addr

--- kubernetes/deploy-stack-for-vagrant-beehive.sh
+++ kubernetes/deploy-stack-for-vagrant-beehive.sh
@@ -6,13 +6,10 @@ mv *.pem ca.pub ssh-key* /etc/waggle
 
 # define beehive endpoints
 export WAGGLE_NODE_ID=0000000000000001
-# not actually using subdomain - just as a standin
-export WAGGLE_BEEHIVE_HOST=beehive.honeyhouse.one
-# not actually using subdomain - just as a standin
-export WAGGLE_BEEHIVE_RABBITMQ_HOST=rabbitmq.honeyhouse.one
+export WAGGLE_BEEHIVE_HOST=10.31.81.200
+export WAGGLE_BEEHIVE_RABBITMQ_HOST=10.31.81.200
 export WAGGLE_BEEHIVE_RABBITMQ_PORT=30000
-# not actually using subdomain - just as a standin
-export WAGGLE_BEEHIVE_UPLOAD_HOST=upload.honeyhouse.one
+export WAGGLE_BEEHIVE_UPLOAD_HOST=10.31.81.200
 export WAGGLE_BEEHIVE_UPLOAD_PORT=30002
 
 # deploy everything!!
