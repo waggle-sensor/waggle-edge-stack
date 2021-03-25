acf437fa5c1e7721fcd7ef2e1b4a8e97cb2feccd
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-25 09:27:38 -0500

    [SAGE-788] Bug Fix: RabbitMQ user wiped on reboot (#5)
    * removed service and shovel users from rabbitmq definitions to prevent password from getting wiped. added external rabbitmq user setup script.
    
    * added volume claim
    
    * fixed shovel username

--- kubernetes/delete-stack.sh
+++ kubernetes/delete-stack.sh
@@ -11,4 +11,3 @@ kubectl delete -f node-exporter.yaml
 # delete config and secrets
 kubectl delete configmap waggle-config
 kubectl delete secret waggle-secret
-kubectl delete secret wes-rabbitmq-service-account-secret
