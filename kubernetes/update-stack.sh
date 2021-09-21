20fc4a43eb48523fb34875e6dc32f7f28ce0c895
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-09-21 12:07:38 -0500

    detect arch in update stack script

--- kubernetes/update-stack.sh
+++ kubernetes/update-stack.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
+fatal() {
+    echo $*
+    exit 1
+}
+
+getarch() {
+    case $(uname -m) in
+    x86_64) echo amd64 ;;
+    aarch64) echo arm64 ;;
+    * ) return 1 ;;
+    esac
+}
+
 # deploy stack
+# TODO clean up bits with different version
 kubectl apply -k .
 
-# TODO clean up bits with different version
+# pull latest compatible version of runplugin
+if ! arch=$(getarch); then
+    fatal "failed to get arch"
+fi
+
+(cd /root && wget -N "https://github.com/sagecontinuum/ses/releases/download/0.6.3/runplugin-${arch}")
