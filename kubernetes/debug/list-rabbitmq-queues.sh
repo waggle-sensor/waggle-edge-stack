e0875e504efb8f83c5df7c148bd0d0f490ee99ad
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-08-11 15:55:30 -0500

    fixed bad script from copying without saving

--- kubernetes/debug/list-rabbitmq-queues.sh
+++ kubernetes/debug/list-rabbitmq-queues.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-set -eu
-
-cd /opt/waggle-edge-stack/kubernetes
-
-echo "removing rabbitmq"
-kubectl delete -f wes-rabbitmq.yaml
-
-echo "removing rabbitmq pvc"
-kubectl delete pvc data-wes-rabbitmq-0
-
-echo "removing data sharing service"
-kubectl delete -f wes-data-sharing-service.yaml
-
-echo "waiting for 60s to allow kubernetes to clean up..."
-sleep 60
-
-echo "redeploying wes"
-./update-stack.sh
+kubectl exec svc/wes-rabbitmq -- rabbitmqadmin list queues
