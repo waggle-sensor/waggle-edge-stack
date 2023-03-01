5e9e6e66137050dcc56931ca6722f3564adfc636
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-03-01 09:46:14 -0600

    added tool to get influxdb out of crash loop so we can lower retention time

---
+++ kubernetes/debug/fix-influxdb-crash-loop-and-update-retention.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! kubectl get pod | grep -q 'wes-node-influxdb-0.*CrashLoopBackOff'; then
+    echo "influxdb doesn't seem to be stuck in crash loop"
+    exit 0
+fi
+
+# temporarily up memory limit so influxdb can run 
+kubectl set resources statefulset/wes-node-influxdb --limits=memory=2Gi
+kubectl delete pod wes-node-influxdb-0
+
+# lower influxdb data retention time to reduce memory usage
+while ! /opt/waggle-edge-stack/kubernetes/debug/update-influxdb-retention.py 3d; do
+    sleep 10
+done
