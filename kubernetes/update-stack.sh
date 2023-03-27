613f6658fb12b845b5c4e7f383f61fbc603c30b5
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-03-27 12:02:25 -0500

    upped ses tools to 0.20.3 to include init container resource fix

--- kubernetes/update-stack.sh
+++ kubernetes/update-stack.sh
@@ -3,7 +3,7 @@ set -e
 
 WAGGLE_CONFIG_DIR="${WAGGLE_CONFIG_DIR:-/etc/waggle}"
 WAGGLE_BIN_DIR="${WAGGLE_BIN_DIR:-/usr/bin}"
-SES_VERSION="${SES_VERSION:-0.20.2}"
+SES_VERSION="${SES_VERSION:-0.22.3}"
 SES_TOOLS="${SES_TOOLS:-runplugin pluginctl sesctl}"
 NODE_MANIFEST_V2="${NODE_MANIFEST_V2:-node-manifest-v2.json}"
 
