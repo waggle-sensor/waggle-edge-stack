d8bd5ce638d7dc6587181bcfc0c61bd59cafb4d4
Sean Shahkarami <sean.shahkarami@gmail.com>
2024-05-09 11:25:17 -0500

    cd into correct dir when running deploy and delete stack

--- kubernetes/delete-stack.sh
+++ kubernetes/delete-stack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $(dirname $0)
 kubectl delete -k wes-app-meta-cache
 kubectl delete -k wes-chirpstack
 kubectl delete -k .
--- kubernetes/deploy-stack.sh
+++ kubernetes/deploy-stack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 # NOTE deploy stack is now "declarative", so it simply does the same thing as updated-stack.sh.
+cd $(dirname $0)
 ./update-stack.sh
